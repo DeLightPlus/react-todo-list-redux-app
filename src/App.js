a84fbf54687efa26b14aa55d7d6dcfe4e18ba883
Kabelo Matlakala <110693341+DeLightPlus@users.noreply.github.com>
2024-10-16 19:04:18 +0200

    react todo list app with redux toolkit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import AddtodoItem  from './components/addtodoItem'
+import TodoList from './components/todolist'
+
+
+function App() 
+{
+  return (
+    <div className="App">
+      <AddtodoItem  />
+      <TodoList/>
+    
+    </div>
+  );
+}
+
+export default App;
